4e51064ae7d7fd73da3ae95c38c249cb24dbdb48
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-23 16:40:12 -0700

    Carrier model

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,2 +1,34 @@
 class Carrier < ActiveRecord::Base
+  helper_method :calculate_usps
+  helper_method :calculate_fedex #:calculate_all_carriers
+
+  # def calculate_all_carriers(zip, items)
+  #
+  # end
+  def calculate_usps(zip, items)
+    BASE_COST = 4
+    distance = (zip - 98104).abs
+    standard = BASE_COST + (items * distance)
+    expedited = standard + 2
+    overnight = standard * 3
+    return [standard, expedited, overnight]
+  end
+
+  def calculate_fedex(zip, items)
+    BASE_COST = 5
+    distance = (zip - 98104).abs
+    standard = BASE_COST + (items * distance)
+    expedited = standard + 2
+    overnight = standard * 3
+    return [standard, expedited, overnight]
+  end
+
+  def calculate_drone(zip, items)
+    BASE_COST = 6
+    distance = (zip - 98104).abs
+    standard = BASE_COST + (items * distance)
+    expedited = standard + 2
+    overnight = standard * 3
+    return [standard, expedited, overnight]
+  end
 end
