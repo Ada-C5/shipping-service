1d6fac2fc360f30702088eb6de9156fafd666f7a
Leah <leahnpetersen@gmail.com>
2016-05-24 15:25:43 -0700

    commented out line in usps class, hello world displaying

--- app/models/UsPostalService.rb
+++ app/models/UsPostalService.rb
@@ -2,5 +2,5 @@ require 'active_shipping'
 
 class USPS
  usps = ActiveShipping::USPS.new(login: ENV['USPS_USERNAME'])
- response = usps.find_rates(origin, destination, packages)
+ # response = usps.find_rates(@origin, @destination, @packages)
 end
