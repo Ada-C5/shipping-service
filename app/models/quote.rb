10169a90dbc0cf8e281a8d2247a8d081ae58ef89
sophia <sophiabaldonado@gmail.com>
2016-05-24 16:20:51 -0700

    Add support for fedex;

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,9 +8,15 @@ class Quote < ActiveRecord::Base
     destination = Quote.destination(address) #change to address when not testing
     packages = Quote.packages
 
-    carrier = parsed_request["carrier"] #set from request
-    usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
-    usps_quote = usps.find_rates(origin, destination, packages)
+    carrier = parsed_request["carrier"].downcase #set from request
+    if carrier = "usps"
+      usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
+      carrier_quote = usps.find_rates(origin, destination, packages)
+    elsif carrier = "fedex"
+      fedex = ActiveShipping::FEDEX.new(login: ENV["FEDEX_USERNAME"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_TEST_KEY"], account: ENV["FEDEX_ACCOUNT_NUMBER"])
+      carrier_quote = fedex.find_rates(origin, destination, packages)
+    end
+
     quote = self.new
     quote.request = request
     quote.response = carrier_quote.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]} #this returns JSON - w
