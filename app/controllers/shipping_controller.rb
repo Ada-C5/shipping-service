0b2c66d1a80d022385a672708369bee75cb76b61
Adriana <dri19tcc@gmail.com>
2016-05-24 14:16:06 -0700

    cleaned up the shipping controller

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -5,10 +5,6 @@ class ShippingController < ApplicationController
     render json: []
   end
 
-  def usps_1
-    render json: []
-  end
-
   def fedex
     fedex = ActiveShipping::FedEx.new(login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"], test: true)
     origin = ActiveShipping::Location.new(country: 'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
@@ -24,17 +20,18 @@ class ShippingController < ApplicationController
       fedex_hash[:fedex_rate_price] = response.rates[0].price
       fedex_array << fedex_hash
     end
-
     render json: fedex_array
   end
 
   def usps
     usps = ActiveShipping::USPS.new(login: ENV["USPS_LOGIN"])
+
     origin = ActiveShipping::Location.new(country: 'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
     destination = ActiveShipping::Location.new(country: 'CA',province: 'ON', city: 'Ottawa', postal_code: 'K1P 1J1')
     packages = [ActiveShipping::Package.new(100, [93,10], cylinder: true), ActiveShipping::Package.new(7.5 * 16, [15, 10, 4.5], units: :imperial)]
 
     response = usps.find_rates(origin, destination, packages)
+
     usps_array = []
     response.rates.length.times do |service_name, rate_price|
       usps_hash = {}
@@ -42,7 +39,6 @@ class ShippingController < ApplicationController
       usps_hash[:usps_rate_price] = response.rates[0].price
       usps_array << usps_hash
     end
-
     render json: usps_array
   end
 end
