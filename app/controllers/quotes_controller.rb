44f6834afbbb31628ffcce45e8b1f98228839ab5
sophia <sophiabaldonado@gmail.com>
2016-05-24 11:33:46 -0700

    Add controller actions, start a get_rates method, update route names;

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,19 @@
 class QuotesController < ApplicationController
+
+  def index
+    quotes = []
+
+    render json: quotes
+  end
+
+  def show
+    carrier = "some carrier"
+    quote = Quote.get_rate(carrier)
+    render json: quotes.as_json(except: response)
+  end
+
+  def selection
+
+  end
+
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,7 @@
 class Quote < ActiveRecord::Base
+
+  def seld.get_rate(carrier)
+    var = ActiveShipping::Carrier
+    self.new(var)
+  end
 end
