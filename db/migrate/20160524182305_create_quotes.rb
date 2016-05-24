2d23d23417c3234f33e59b0d8890883cf85bb6a6
sophia <sophiabaldonado@gmail.com>
2016-05-24 14:08:50 -0700

    Created db schema and migrated;

--- db/migrate/20160524182305_create_quotes.rb
+++ db/migrate/20160524182305_create_quotes.rb
@@ -1,6 +1,9 @@
 class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
+      t.string :request
+      t.string :response
+      t.string :selection
 
       t.timestamps null: false
     end
