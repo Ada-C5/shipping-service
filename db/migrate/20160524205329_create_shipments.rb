6bae6a2427643afe60cdaf0a61fdb00709f41b18
Nadine Curry <nlcurry@gmail.com>
2016-05-24 15:52:21 -0700

    setup shipment model

--- db/migrate/20160524205329_create_shipments.rb
+++ db/migrate/20160524205329_create_shipments.rb
@@ -1,6 +1,10 @@
 class CreateShipments < ActiveRecord::Migration
   def change
     create_table :shipments do |t|
+      t.string :order_id
+      t.string :carrier
+      t.decimal :rate
+      t.string :delivery
 
       t.timestamps null: false
     end
