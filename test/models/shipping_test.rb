3fecaa6cf3def3b2806b64f1ee334eecc6e38086
Valerie <val.conklin@gmail.com>
2016-05-24 11:23:27 -0700

    USPS test works

--- test/models/shipping_test.rb
+++ test/models/shipping_test.rb
@@ -3,10 +3,6 @@ require 'test_helper'
 class ShippingTest < ActiveSupport::TestCase
   describe "API" do
 
-    # setup do
-    #
-    # end
-
     it "should return an instance of USPS", :vcr do
       usps  = ActiveShipping::USPS.new(login: ENV["USPS_LOGIN_KEY"])
       assert usps.is_a? ActiveShipping::USPS
