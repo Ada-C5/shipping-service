730f658c5ecd76277dd3559d1ce6c40dfb6b2578
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-24 15:01:30 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :shippings, only: [:index, :show] do
-    collection do
-      post 'rates'
-    end
-  end
+  post '/shipping/rates' => 'shipping#rates'
+  resources :shipping, only: [:index, :show]
+
+  # post '/pets/search' => 'pets#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
