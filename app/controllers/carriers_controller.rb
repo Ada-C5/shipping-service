347e7f527c8c4067bcfef817465868b48a11949d
Jade Vance <jadenicolevance@gmail.com>
2016-05-23 22:41:07 -0700

    added notes for tomorrow

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -1,2 +1,9 @@
 class CarriersController < ApplicationController
+
+  def index
+    # I think this should point to an endpoint such as:
+    # http://suchhipsterly.herokuapp.com/shipping
+    # routing needs to be considered
+  end 
+
 end
--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,2 +1,17 @@
 class Carrier < ActiveRecord::Base
+
+  # stuff to care about:
+    # we need packages, origin info, destination info and the rate
+
+  
+
+  # Using the ActiveShipping gem: 
+  # def self.create_ups_instance
+  #   ActiveShipping::UPS.new(login: ENV["UPS_LOGIN"], password: ENV["UPS_PASSWORD"], key: ENV['UPS_ACCESS_KEY'])
+  # end
+
+  # def self.create_fedex_instance
+  #   ActiveShipping::FEDEX.new(login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_ACCESS_KEY"], account:ENV["FEDEX_ACCOUNT"] )
+  # end
+
 end
