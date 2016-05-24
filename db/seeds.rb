e2a8baba236bc3e789e749bc387b57317d4f54b5
Adriana <dri19tcc@gmail.com>
2016-05-24 14:33:08 -0700

    working on testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ShippingController.fedex.create(fedex_service_name: "FedEx International Priority", fedex_rate_price: 2245)
+ShippingController.fedex.create(fedex_service_name: "FedEx Express", fedex_rate_price: 11188)
+ShippingController.fedex.create(fedex_service_name: "FedEx Priority", fedex_rate_price: 8135)
