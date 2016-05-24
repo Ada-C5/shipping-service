5252ec5b4933ccec49a735bd9aa6fae17c49fe8b
sophia <sophiabaldonado@gmail.com>
2016-05-24 11:24:06 -0700

    Setup pg db, generate controller and model for quotes, set routes;

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # resources :quotes, only: [:show, :index]
+  get '/quotes'           => 'quotes#index'
+  get '/quotes/:carrier'  => 'quotes#show'
+  post '/quotes/:carrier' => 'quotes#selection'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
