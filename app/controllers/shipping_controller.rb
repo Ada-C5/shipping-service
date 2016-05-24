19a9bad40fbcac6c1d57d92d23a3ab8228c4487b
Adriana <dri19tcc@gmail.com>
2016-05-23 22:32:39 -0700

    added fedex keys and login info

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -2,4 +2,14 @@ class ShippingController < ApplicationController
   def quotes
     render json: []
   end
+
+  def fedex
+    fedex = ActiveShipping::FedEx.new(login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"])
+    
+    tracking_info = fedex.find_tracking_info('tracking-number', carrier_code: 'fedex_ground') # Ground package
+
+    tracking_info.shipment_events.each do |event|
+      puts "#{event.name} at #{event.location.city}, #{event.location.state} on #{event.time}. #{event.message}"
+    end
+  end
 end
