2415d8f85b1708ba1c2b6838333c1b30b9bf8048
sophia <sophiabaldonado@gmail.com>
2016-05-24 16:19:26 -0700

    Start testing show action for quotes controller;

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
-class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+module QuotesControllerTest
+  class ShowAction < ActionController::TestCase
+    setup do
+      @request.headers['Accept'] = Mime::JSON
+      @request.headers['Content-Type'] = Mime::JSON.to_s
+
+      get :show, carrier: "USPS", shipping: {
+            "carrier": "USPS",
+            "address": { country: "US", state: "WA", city: "Seattle", zip: "98122" }
+          }.to_json
+
+      # @body = JSON.parse(@response.body)
+    end
+
+    test "can get #show" do
+      assert_response :success
+    end
+
+    # test "it should return a Quote object" do
+    #   assert_instance_of JSON, @body
+    # end
+
+  end
+
+
 end
