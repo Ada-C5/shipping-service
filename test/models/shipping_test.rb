e114a623c1ff38a07225eb672c68cbc23bd846b9
Valerie <val.conklin@gmail.com>
2016-05-24 11:32:17 -0700

    Somehow lost the USPS test in some weird merge thing, but luckily I had given that code to someone on Slack so I was able to paste it back in, whoops. This commit contains that USPS test code.

--- test/models/shipping_test.rb
+++ test/models/shipping_test.rb
@@ -3,18 +3,16 @@ require 'test_helper'
 class ShippingTest < ActiveSupport::TestCase
   describe "API" do
 
-    # setup do
-    #
-    # end
+    it "should return an instance of USPS", :vcr do
+      usps  = ActiveShipping::USPS.new(login: ENV["USPS_LOGIN_KEY"])
+      assert usps.is_a? ActiveShipping::USPS
+    end
 
-    it "shuld return an instance of Fedex", :vcr do
+    it "should return an instance of Fedex", :vcr do
       fedex = ActiveShipping::FedEx.new(login: ENV['FEDEX_METER_NUMBER'],
       password: ENV['FEDEX_TEST_PASSWORD'], key: ENV['FEDEX_TEST_KEY'], account: ENV['FEDEX_ACCOUNT_NUMBER'])
       assert fedex.is_a? ActiveShipping::FedEx
     end
-
-
-    it ""
 
   end
 end
