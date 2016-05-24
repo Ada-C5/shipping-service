ec9b93b5b656c49445aaf9450d9a6c139d0e1753
Jade Vance <jadenicolevance@gmail.com>
2016-05-24 15:53:00 -0700

    organized the carrier model into methods to be privately called by the class

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,42 +1,48 @@
 class Carrier < ActiveRecord::Base
 
-    # stuff to care about:
-    # we need packages, origin info, destination info and the rate
-    
-    # get dem instances 
-    create_ups_instance
-    create_fedex_instance
+  # stuff to care about:
+  # we need packages, origin info, destination info and the rate
+  
+  # get dem instances 
+  ups        = create_ups_instance
+  fedex      = create_fedex_instance
+  
+  #get dem quotes, instance variables for easy passing between model and view  
+  @ups_rate   = carrier_quotes(ups)
+  @fedex_rate = carrier_quotes(fedex)
 
+  private 
+  
+  def self.create_ups_instance
+    ups = ActiveShipping::UPS.new(login: ENV["UPS_LOGIN"], password: ENV["UPS_PASSWORD"], key: ENV['UPS_ACCESS_KEY'])
+  end
+
+  def self.create_fedex_instance
+    fedex = ActiveShipping::FedEx.new(login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"],key: ENV["FEDEX_ACCESS_KEY"], account: ENV["FEDEX_ACCOUNT"])
+  end
+
+  def carrier_quotes(carrier)
     # this could be a constant 
-    origin = ActiveShipping::Location.new(country: 'US',
-                                          state: 'WA',
-                                          city: 'Seattle',
-                                          zip: '98118')
+    origin      = ActiveShipping::Location.new(country: 'US',
+                                               state: 'WA',
+                                               city: 'Seattle',
+                                               zip: '98118')
 
+    # this is pseudocode for what the params may look like when it comes in 
     destination = ActiveShipping::Location.new(country: params[:country],
                                                state: params[:state],
                                                city: params[:city],
                                                zip: params[:zip])
 
     # could be an array if we have multiple packages. 
-    packages = ActiveShipping::Package.new(5 * 16,          # 7.5 lbs, times 16 oz/lb.
-                                 [12, 12, 3],     # 15x10x4.5 inches
-                                 units: :imperial)  # not grams, not centimetres
+    package     = ActiveShipping::Package.new(5 * 16,           # 5 lbs, times 16 oz/lb.
+                                             [12, 12, 3],       # 12x12x3 inches, not centimeters
+                                             units: :imperial)  # not grams
   
     # contact active_shipping with pieces of info 
-    get_rates = ups.find_rates(origin, destination, packages)
+    response    = carrier.find_rates(origin, destination, package)
 
     # sorts by price and type, also .collect gives us access to enumerate over the collection
-    sort_rates = ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-
-    private 
-    # Using the ActiveShipping gem: 
-    def self.create_ups_instance
-      ups = ActiveShipping::UPS.new(login: ENV["UPS_LOGIN"], password: ENV["UPS_PASSWORD"], key: ENV['UPS_ACCESS_KEY'])
-    end
-
-    def self.create_fedex_instance
-      fedex = ActiveShipping::FedEx.new(login: ENV["FEDEX_LOGIN"], password: ENV["FEDEX_PASSWORD"],key: ENV["FEDEX_ACCESS_KEY"], account: ENV["FEDEX_ACCOUNT"])
-    end
-
+    sort_rates  = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+  end 
 end
