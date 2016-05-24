db4035cffafcc9a477e4ffca136405e9cd6ca8b6
sophia <sophiabaldonado@gmail.com>
2016-05-24 14:08:31 -0700

    Added methods in model to communicate with ActiveShipping gem;

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,37 @@
 class Quote < ActiveRecord::Base
 
-  def seld.get_rate(carrier)
-    var = ActiveShipping::Carrier
-    self.new(var)
+
+
+
+  def self.get_rate(carrier, address)
+    origin = Quote.origin
+    address1 = {
+      country: "US",
+      state: "WA",
+      city: "Seattle",
+      zip: "98122"
+    }
+    destination = Quote.destination(address1)
+    packages = Quote.packages
+
+    usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
+    usps_quote = usps.find_rates(origin, destination, packages)
+    quote = self.new
+    quote.response = usps_quote
+    quote.save
+    quote.response
   end
+
+  def self.origin
+    ActiveShipping::Location.new(country: "US", state: "WA", city: "Seattle", zip: "98121")
+  end
+
+  def self.destination(address)
+    ActiveShipping::Location.new(country: address[:country], state: address[:state], city: address[:city], zip: address[:zip])
+  end
+
+  def self.packages
+    ActiveShipping::Package.new(7, [15, 10, 4.5], units: :imperial)
+  end
+
 end
