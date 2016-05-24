afa456dab6745a0242a7da32b1640800701318f8
Adriana <dri19tcc@gmail.com>
2016-05-24 10:34:10 -0700

    added config to application.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/quotes' => 'shipping#quotes'
   get '/usps' => 'shipping#usps', as: "usps"
+  get '/fedex' => 'shipping#fedex', as: "fedex"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
