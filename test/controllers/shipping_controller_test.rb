03e55d5a23e1d0ebe15dfe73bf2a5d792dc8c236
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-24 15:02:11 -0700

    updated test

--- test/controllers/shipping_controller_test.rb
+++ test/controllers/shipping_controller_test.rb
@@ -4,14 +4,21 @@ class ShippingControllerTest < ActionController::TestCase
   setup do
       @request.headers['Accept'] = Mime::JSON
       @request.headers['Content-Type'] = Mime::JSON.to_s
+      @params = {
+        order: { city: "Seattle", state: "WA", country: "US", zip: "98112",
+        origin: { state: "WA", zip: '98112', city: "Seattle" },
+        orderitems: [{ height: 4, width: 6, length: 10, weight: 30}]
+        }
+      }
 
-      post :search, order: { city: "Seattle", state: "WA", country: "US", zip: "98112",
-           orderitems: { height: 4, width: 6, length: 10, weight: 30} }
-      @body = JSON.parse(response.body)
     end
 
     test "response should be an array" do
+      post :rates, @params, :format => 'json'
+        @body = JSON.parse(response.body)
+
+        puts @body
       assert @body.class, Array
-    end 
+    end
 
 end
