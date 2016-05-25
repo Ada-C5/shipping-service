1a8c97e5fea798b74ce0138d7ba63f1e6cb49851
Cristal Tay <bluecolorpencils@gmail.com>
2016-05-24 17:06:24 -0700

    added an info method to the shipping controller. Also added a route to see if we can talk to the betsy host and retrieve the json.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+get 'product' => 'shippings#info'
   # Example resource route with options:
   #   resources :products do
   #     member do
