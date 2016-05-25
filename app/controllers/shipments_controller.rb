32eebc160fe321aef1d7591358917eac8a81930e
Nadine Curry <nlcurry@gmail.com>
2016-05-25 15:14:53 -0700

    notes

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,2 +1,14 @@
 class ShipmentsController < ApplicationController
+  def index
+  shipments = Shipment.all
+  render json: shipments.as_json(except: [:created_at, :updated_at])
+  # render json: []
+  #
+  end
+
+  def ups
+
+  end
+
+  def fedex
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   'products'
   '/products/:id'
+  # need to confirm we'll be working with products instead of orderitems
+  # unfinished routes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
