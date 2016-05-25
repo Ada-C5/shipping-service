c3fcd3dd70736051f1aca342a73b8c54cc23d9c4
melissajimison <melissajimison@gmail.com>
2016-05-25 15:46:57 -0700

    set the logic for fetching the data fon UPS and FedEx API's

--- lib/shippingwrapper.rb
+++ lib/shippingwrapper.rb
@@ -1,11 +1,30 @@
 require 'active_shipping'
 
 module ShippingWrapper
-  def self.login_ups
+  def self.create_fedex(origin, destination, packages )
     ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
+    origin = ActiveShipping::Location.new(origin)
+
+    destination = ActiveShipping::Location.new(destination)
+
+    packages = packages.map do |product|
+      ActiveShipping::Package.new(product[:weight_lbs] * 16, [product[:length_in], product[:height_in], product[:width_in]],units: product[:units])
+    end
+
+    ups_info.find_rates(origin, destination, packages )
   end
 
-  def self.login_fedex
-    ActiveShipping::UPS.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PASSWORD'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCOUNT'], test: ENV['FEDEX_TEST'])
+  def self.create_fedex(origin, destination, packages )
+    fedex_info = ActiveShipping::FedEx.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PASSWORD'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCOUNT'], test: ENV['FEDEX_TEST'])
+
+    origin = ActiveShipping::Location.new(origin)
+
+    destination = ActiveShipping::Location.new(destination)
+
+    packages = packages.map do |product|
+      ActiveShipping::Package.new(product[:weight_lbs] * 16, [product[:length_in], product[:height_in], product[:width_in]],units: product[:units])
+    end
+
+    fedex_info.find_rates(origin, destination, packages )
   end
 end
