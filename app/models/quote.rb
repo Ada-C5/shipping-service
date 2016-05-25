c2b8e1bd0df0fda4796e0247b6eb16c88e4575be
sophia <sophiabaldonado@gmail.com>
2016-05-25 10:07:31 -0700

    Return quote instance from get_rate instead of true; Make method names more descriptive;

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,17 +2,17 @@ class Quote < ActiveRecord::Base
 
   def self.get_rate(request)
     parsed_request = JSON.parse(request)
-    origin = Quote.origin
+    origin = Quote.get_origin
 
     address = parsed_request["address"]
-    destination = Quote.destination(address) #change to address when not testing
-    packages = Quote.packages
+    destination = Quote.get_destination(address) #change to address when not testing
+    packages = Quote.get_packages
 
     carrier = parsed_request["carrier"].downcase #set from request
-    if carrier = "usps"
+    if carrier == "usps"
       usps = ActiveShipping::USPS.new(login: ENV["USPS_USERNAME"], password: ENV["USPS_PASSWORD"])
       carrier_quote = usps.find_rates(origin, destination, packages)
-    elsif carrier = "fedex"
+    elsif carrier == "fedex"
       fedex = ActiveShipping::FEDEX.new(login: ENV["FEDEX_USERNAME"], password: ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_TEST_KEY"], account: ENV["FEDEX_ACCOUNT_NUMBER"])
       carrier_quote = fedex.find_rates(origin, destination, packages)
     end
@@ -21,18 +21,18 @@ class Quote < ActiveRecord::Base
     quote.request = request
     quote.response = carrier_quote.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]} #this returns JSON - w
     quote.save
-
+    quote
   end
 
-  def self.origin
+  def self.get_origin
     ActiveShipping::Location.new(country: "US", state: "WA", city: "Seattle", zip: "98121")
   end
 
-  def self.destination(address)
+  def self.get_destination(address)
     ActiveShipping::Location.new(country: address["country"], state: address["state"], city: address["city"], zip: address["zip"])
   end
 
-  def self.packages
+  def self.get_packages
     ActiveShipping::Package.new(7, [15, 10, 4.5], units: :imperial)
   end
 
