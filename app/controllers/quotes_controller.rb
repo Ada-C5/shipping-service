8b8cb3777c339a8314fe88eddd8905a26998f6bf
Valerie <val.conklin@gmail.com>
2016-05-25 09:11:14 -0700

    Set up quotes controller and route and index method for that

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,9 @@
+class QuotesController < ApplicationController
+
+  def index
+    @packages = [
+        ActiveShipping::Package.new(params[:something][:height], [93,10], cylinder: true),
+        ActiveShipping::Package.new(7.5 * 16, [15, 10, 4.5], units: :imperial)
+        ]
+  end
+end
