5049b0090fd87a303be6d20eeabfa73ba3615a3d
Sarah <sarahkane682@yahoo.com>
2016-05-25 15:39:45 -0700

    working on quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,14 +1,13 @@
 class QuotesController < ApplicationController
 
   def index
-    JSON.parse
-
 
     @package = ActiveShipping::Package.new(params[:product][:weight],[params[:product][:width], params[:product][:height]])
 
     @origin = ActiveShipping::Location.new(params[:origin][:street_address],params[:origin][:city],params[:origin][:state],params[:origin][:zip])
 
     @destination = ActiveShipping::Location.new(params[:destination][:street_address],params[:destination][:city],params[:destination][:state],params[:destination][:zip])
-  
   end
+#data base code
+
 end
