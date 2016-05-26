c42a7bb49adbec42e3226405f2b2cc3f41fded50
Adriana <dri19tcc@gmail.com>
2016-05-26 15:38:11 -0700

    Deleted comments

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -12,5 +12,3 @@ class ShippingController < ApplicationController
     Log.create(betsy_json_query: (@_request), betsy_json_response: @_response, betsy_order_id: order_id)
   end
 end
- # Generate new model, called log.rb?  this would then be the database
- # where we keep each log.  Whenever quotes made, create
