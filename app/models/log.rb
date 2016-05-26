1ab44e3382a2705d058f7a7c1d33f9a7db9d4116
Valerie <val.conklin@gmail.com>
2016-05-26 09:29:40 -0700

    Started setup for log table in database

---
+++ app/models/log.rb
@@ -0,0 +1,7 @@
+class Log < ActiveRecord::Base
+
+  #should log, in a database, all requests for shipping information (from betsy, in our case) and all composed responses.
+  # This can be a single database table that stores the request json from betsy and the response json sent back to betsy, one row per request.
+
+
+end
