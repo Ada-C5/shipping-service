7dba108420a95268db2092f6b32d1a362f05bf79
Jade Vance <jadenicolevance@gmail.com>
2016-05-26 09:58:55 -0700

    updated carrier information

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -6,7 +6,7 @@ class Carrier < ActiveRecord::Base
   # logging?? 
   def save_data
     carrier          = Carrier.new 
-    carrier.name     = #carrier_name
+    carrier.name     = # who the request came from (hipsterly)
     carrier.request  = # stuff from betsy
     carrier.response = # shipping selection from betsy
     carrier.save # save that nonsese 
