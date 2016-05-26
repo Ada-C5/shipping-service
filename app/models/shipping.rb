5c1fe11013c58b401a06e9b27575c8da45663de2
Valerie <val.conklin@gmail.com>
2016-05-26 11:06:41 -0700

    Working on getting shipping quote showing

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -11,15 +11,23 @@ class Shipping < ActiveRecord::Base
   end
 
   def self.create_by_params(params)
-    package = ActiveShipping::Package.new(params[:product][:weight],[params[:product][:width], params[:product][:height]])
-    origin = ActiveShipping::Location.new( params[:origin].merge(:country => "US"))
-    destination = ActiveShipping::Location.new(params[:destination].merge(:country => "US"))
+
+    package_info = params.keys[0] # this is a string
+    params = JSON.parse(package_info)
+    params["origin"]["zip"] = params["origin"]["zip"].to_i
+    # binding.pry
+    params["destination"]["zip"] = params["destination"]["zip"].to_i
+    package = ActiveShipping::Package.new(params["product"]["weight"],[params["product"]["width"], params["product"]["height"]])
+
+    origin = ActiveShipping::Location.new( params["origin"].merge("country" => "US"))
+    destination = ActiveShipping::Location.new(params["destination"].merge("country" => "US"))
     Shipping.new(package,origin,destination)
   end
 
 
   def find_rates
     [@usps,@fedex].map do |carrier|
+      binding.pry
       carrier.find_rates(@origin,@destination,@package).rates.sort_by(&:price).map do |rate|
         {service_name: rate.service_name, price: rate.price}
        end
