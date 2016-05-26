dc9334f86aa6ba7b58f83a7f2f9bfd4183adc563
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-26 14:27:23 -0700

    V1 of log, saves all incoming info and response

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -7,16 +7,18 @@ class CarriersController < ApplicationController
     estimate = {
       "usps" => Carrier.estimate_usps_shipping(items, state, city, zip),
       "ups" => Carrier.estimate_ups_shipping(items, state, city, zip)}
+    log = "#{city}, #{state}, #{zip}, #{items}, #{estimate}"
+    Carrier.create(request: log)
     render json: estimate.as_json, :status => :ok
   end
 
   def selected
-    carrier = params[:name]
-    zip = params[:zip]
-    items = params[:items]
-  end
-
-  def tracking
-
+    carrier = params[:carrier_type]
+    price = params[:carrier_price]
+    number = rand(99999) + price.to_i
+    tracking = {"#{carrier}" => "#{number}"}
+    log = "#{price}, #{carrier}, #{tracking}"
+    Carrier.create(request: log)
+    render json: tracking.as_json, :status => :ok
   end
 end
