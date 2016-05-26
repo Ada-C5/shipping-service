ba68611fcd89d253edc1ff633ea0bcbf63194fbb
Adriana <dri19tcc@gmail.com>
2016-05-26 08:54:53 -0700

    Added notes on directions we can go

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -7,7 +7,10 @@ class ShippingController < ApplicationController
     fedex = ShippingWrapper.fedex(params[:zipcode], params[:quantity])
     usps = ShippingWrapper.usps(params[:zipcode], params[:quantity])
     join = {fedex: fedex, usps: usps}
+    # Log.create add new and saves it to the database
     # fun = {zipcode: params[:zipcode], quantity: params[:quantity]} #this is for the view test
     render json: join
   end
 end
+ # Generate new model, called log.rb?  this would then be the database
+ # where we keep each log.  Whenever quotes made, create
