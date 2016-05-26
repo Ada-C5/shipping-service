f85a5923de70d7ac4bce0954a897b02a134b853f
Sarah <sarahkane682@yahoo.com>
2016-05-25 18:42:25 -0700

    quote controller uses quote model

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,8 @@
 class QuotesController < ApplicationController
 
   def index
-
-    @package = ActiveShipping::Package.new(params[:product][:weight],[params[:product][:width], params[:product][:height]])
-
-    @origin = ActiveShipping::Location.new(params[:origin][:street_address],params[:origin][:city],params[:origin][:state],params[:origin][:zip])
-
-    @destination = ActiveShipping::Location.new(params[:destination][:street_address],params[:destination][:city],params[:destination][:state],params[:destination][:zip])
+    rate_quote = Shipping.create_by_params(params)
+    rates = rate_quote.find_rates
+    render json: rates.to_json
   end
-#data base code
-
 end
