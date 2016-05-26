9e3dfaa6b61801302cc538d96f013e00df773542
melissajimison <melissajimison@gmail.com>
2016-05-26 09:10:45 -0700

    cleaned the code that is not being used

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -8,54 +8,14 @@ class ShippingsController < ApplicationController
   end
 
   def info
-
     data_packages = params[:products_specs]
     data_destination = params[:destination]
     data_origin = params[:origin]
 
-    # binding.pry
-    total_result = ShippingWrapper.create_ups(data_origin, data_destination, data_packages)
-
-    # ups_result = ShippingWrapper.create_fedex(data_origin, data_destination, data_packages)
-    # total_result = {
-      # fedex_result: fedex_result,
-    #   ups_result: ups_result
-    # }
-    # binding.pry
+    total_result = ShippingWrapper.create(data_origin, data_destination, data_packages)
 
     render json: total_result.as_json
-    # render json: params.as_json
-    # render json:
-    # , status: :no_content
-    # end
+
   end
- #
 
- # origin = ActiveShipping::Location.new(country: 'US',
- #                                       state: 'CA',
- #                                       city: 'Beverly Hills',
- #                                       zip: '90210')
- #
- # destination = ActiveShipping::Location.new(country: 'CA',
- #                                            province: 'ON',
- #                                            city: 'Ottawa',
- #                                            postal_code: 'K1P 1J1')
-
-
-#shippings
-#provider (UPS/FedEx)
-#types
-#cost
-#tracking
-#delivery
-
-
-#This is what we need to save in our API.
-#id
-#address
-#address2
-#city
-#state
-#zipcode
-#response
 end
--- lib/shippingwrapper.rb
+++ lib/shippingwrapper.rb
@@ -8,7 +8,7 @@ class ShippingWrapper
   end
 
 
-  def self.create_ups(origin, destination, packages )
+  def self.create(origin, destination, packages )
     ups_info = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
     fedex_info = ActiveShipping::FedEx.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PASSWORD'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCOUNT'], test: ENV['FEDEX_TEST'])
 
@@ -19,23 +19,12 @@ class ShippingWrapper
     packages = packages.map do |product|
       ActiveShipping::Package.new(product[:weight_lbs] * 16, [product[:length_in], product[:height_in], product[:width_in]],units: product[:units])
     end
+
     fedex_data = fedex_info.find_rates(origin, destination, packages)
     ups_data = ups_info.find_rates(origin, destination, packages)
+
     return self.new(fedex_data, ups_data)
 
   end
 
-  def self.create_fedex(origin, destination, packages )
-    fedex_info = ActiveShipping::FedEx.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PASSWORD'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCOUNT'], test: ENV['FEDEX_TEST'])
-    fedex_info.find_rates(origin, destination, packages )
-
-    origin = ActiveShipping::Location.new(origin)
-
-    destination = ActiveShipping::Location.new(destination)
-
-    packages = packages.map do |product|
-      ActiveShipping::Package.new(product[:weight_lbs] * 16, [product[:length_in], product[:height_in], product[:width_in]],units: product[:units])
-    end
-
-  end
 end
