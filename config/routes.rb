d4c74274257213b8589cca82f214c0db7d1ca745
Sarah <sarahkane682@yahoo.com>
2016-05-25 18:43:42 -0700

    changed get to post for quote route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get '/quote' => 'quotes#index'
+  post '/quote' => 'quotes#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
