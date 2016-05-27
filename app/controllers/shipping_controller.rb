c750ae65e549679e3251cd989ee8af7efb5c4246
justine <justine.winnie@gmail.com>
2016-05-27 15:25:54 -0700

    hrmmm....

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -8,11 +8,14 @@ class ShippingController < ApplicationController
   end
 
   def quotes
-    fedex = ShippingWrapper.fedex(params[:zipcode], params[:quantity])
-    usps = ShippingWrapper.usps(params[:zipcode], params[:quantity])
-    order_id = params[:order_id]
-    join = {fedex: fedex, usps: usps}
-    render json: join.as_json(except: [:updated_at])
-    Log.create(betsy_json_query: (@_request), betsy_json_response: @_response, betsy_order_id: order_id)
+    if params[:zipcode]
+      fedex = ShippingWrapper.fedex(params[:zipcode], params[:quantity])
+      usps = ShippingWrapper.usps(params[:zipcode], params[:quantity])
+      order_id = params[:order_id]
+      join = {fedex: fedex, usps: usps}
+      render json: join.as_json(except: [:updated_at])
+      Log.create(betsy_json_query: (@_request), betsy_json_response: @_response, betsy_order_id: order_id)
+    else
+      render json: [], status: :no_content
   end
 end
